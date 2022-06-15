55c05031c16c0100380fc5b09c7a9a51218dec5d
Alaa Aldandachi <58466561+AlaaDan@users.noreply.github.com>
2022-06-15 10:04:04 +0000

    Added a while loop asking the user if he wants to go again

--- main.py
+++ main.py
@@ -3,11 +3,7 @@ from art import logo
 print(logo)
 mode = True
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-if shift > 26:
-  shift = shift%26
+
 
 def ceasar(direction, text, shift):
   if direction  == "encode":
@@ -16,6 +12,7 @@ def ceasar(direction, text, shift):
     for char in text:
       if char in alphabet:
         position = alphabet.index(char)
+      
         if position > 20 and position+shift > 25:
           new_position= shift-1
           cipher_code+=alphabet[new_position]
@@ -42,11 +39,24 @@ def ceasar(direction, text, shift):
         cipher_code+= char
     
     print(f'The decoded text is: {cipher_code}')
-
-
-
-ceasar(direction, text, shift)
+    
+on = True
+while on:
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+  if shift > 26:
+    shift = shift%26
   
+  ceasar(direction, text, shift)
+  repeat = input("If you want to go again, Type: 'yes'. Otherwise Type: 'no'.").lower()
+  if repeat != "no" and repeat != "yes":
+    on = False
+    print("Thanks for using the Caesar Cipher.")
+  elif repeat == "no":
+    on = False
+    print("Thanks for using the Caesar Cipher.")
+    
   
  
 
